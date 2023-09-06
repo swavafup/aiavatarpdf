a7153592a2dba85991e3c4f4b9b4aa88b97609c2
Milestone AI <47553718+swavafup@users.noreply.github.com>
2023-09-06 16:03:22 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-<htmal>
+<html>
 <head>
     <script src='https://cdn.firebase.com/js/client/2.2.1/firebase.js'></script>
     <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
