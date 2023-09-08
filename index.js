18da9aefe1a8481f91d9c7c276b36eef8b7eddf0
Milestone AI <47553718+swavafup@users.noreply.github.com>
2023-09-08 09:21:33 +0400

    Update index.js

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-console.log(firebaseApp.auth());
+console.log(firebaseApp);
 
 const email = "swavaf3693@gmail.com";
 const password = "Swavaf@123";
