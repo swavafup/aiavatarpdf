2d10b4c54f1032f1bff5c219c85ad22aa7da6319
Milestone Lab ⛵ <47553718+swavafup@users.noreply.github.com>
2023-10-12 15:34:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,100 @@ webApp.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/database');
+
+const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
+const { getDatabase, ref, get, set} = require("firebase/database");
+
+
+
+
+firebaseConfig = {
+    'apiKey': "AIzaSyC_1yhveazgVtX-hfmZh6OwFGvODNgCgG4",
+    'authDomain': "loginwithstreamlit.firebaseapp.com",
+    'projectId': "loginwithstreamlit",
+    'databaseURL': "https://loginwithstreamlit-default-rtdb.firebaseio.com",
+    'storageBucket': "loginwithstreamlit.appspot.com",
+    'messagingSenderId': "286638028806",
+    'appId': "1:286638028806:web:931ff9cffb9421e4b42b87",
+    'measurementId': "G-SFTNJ19HS6"
+}
+
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+const auth = getAuth(firebaseApp);
+// console.log(auth);
+
+const email = "swavaf3693@gmail.com";
+const password = "Swavaf@123";
+
+
+
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // User is signed in
+    const user = userCredential.user;
+    console.log("User signed in:", user);
+  })
+  .catch((error) => {
+    // Handle errors
+    console.error("Sign-in error:", error);
+  });
+
+// Reference to the database
+// const database = firebaseApp.database();
+
+const user = auth.currentUser;
+
+
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+  // Assuming you have stored the user's unique identifier (UID) in your database under "users"
+        const userId = user.uid;
+  
+	
+  // Reference to the user's data
+        const database = getDatabase();
+        const userRef = ref(database, userId);
+        console.log(userRef);
+        const childPath = "request"; // Replace with the actual child node name
+
+// Create a reference to the child location
+  // const childRef = ref(userRef, childPath);
+
+        get(userRef)
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              const userData = snapshot.val();
+              console.log(userData.request);
+	      
+		      const userRef = ref(database, userId); // Ensure you have the correct userRef
+      	      set(userRef, newData)
+                .then(() => {
+                  console.log('Data updated successfully.');
+                })
+                .catch((error) => {
+                  console.error('Error setting data:', error);
+                });
+            } else {
+              console.log('No data available for this user.');
+            }
+         })
+          .catch((error) => {
+            console.error('Error getting data:', error);
+          });
+
+
+      } else {
+        console.log("User is not authenticated. Please log in first.");
+      }
+      })
+
 const apiKey = 'sec_qT39IlsF7TNBBS8Q2GNomWd9vpcSzYHN'; // Replace with your API key
 const sourceId = 'src_g69WoiZ85Mdh52ziav7PM'; // Replace with your source ID
 
