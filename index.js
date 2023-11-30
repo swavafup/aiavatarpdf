ea5c369ee0382eca283d800315c33fafc35f18c5
Milestone Lab ⛵ <47553718+swavafup@users.noreply.github.com>
2023-11-30 16:27:00 +0400

    Update index.js

--- index.js
+++ index.js
@@ -37,75 +37,6 @@ firebaseConfig = {
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-
-// const auth = getAuth(firebaseApp);
-// // console.log(auth);
-
-// const email = "swavaf3693@gmail.com";
-// const password = "Swavaf@123";
-
-// let sourceidnew = "";
-
-
-// signInWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // User is signed in
-//     const user = userCredential.user;
-//     console.log("User signed in:", user);
-//   })
-//   .catch((error) => {
-//     // Handle errors
-//     console.error("Sign-in error:", error);
-//   });
-
-// // Reference to the database
-// // const database = firebaseApp.database();
-
-// const user = auth.currentUser;
-
-
-//     auth.onAuthStateChanged((user) => {
-//       if (user) {
-//   // Assuming you have stored the user's unique identifier (UID) in your database under "users"
-//         const userId = user.uid;
-  
-	
-//   // Reference to the user's data
-//         const database = getDatabase();
-//         const userRef = ref(database, userId);
-//         console.log(userRef);
-//         const childPath = "request"; // Replace with the actual child node name
-
-// // Create a reference to the child location
-//   // const childRef = ref(userRef, childPath);
-
-//         get(userRef)
-//           .then((snapshot) => {
-//             if (snapshot.exists()) {
-//               const userData = snapshot.val();
-// 	      sourceidnew = userData.request
-// 	      console.log(sourceidnew);
-      	    
-//             } else {
-//               console.log('No data available for this user.');
-//             }
-//          })
-//           .catch((error) => {
-//             console.error('Error getting data:', error);
-//           });
-
-
-//       } else {
-//         console.log("User is not authenticated. Please log in first.");
-//       }
-//       })
-
-// const apiKey = 'sec_qT39IlsF7TNBBS8Q2GNomWd9vpcSzYHN'; // Replace with your API key
-// const sourceId = sourceidnew
-// console.log("sourceId from firebase");
-// console.log(sourceId);
-// // const sourceId = 'src_g69WoiZ85Mdh52ziav7PM'; // Replace with your source ID
-
 webApp.get('/', (req, res) => {
     res.sendStatus(200);
 });
@@ -121,9 +52,6 @@ webApp.post('/dialogflow', async (req, res) => {
 	
     // const email = "admin@gmail.com";
     // const password = "admin@123";
-
-    
-
 
 signInWithEmailAndPassword(auth, "irirobotictslab@uaeu.com", "iriroboticslab@123")
   .then((userCredential) => {
